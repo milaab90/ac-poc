6116be7627b93b09c155da92f328ab24ccde216b
Camila Barreto <camila.camilabarreto@gmail.com>
2020-10-03 17:57:27 -0300

    [dev] filters... finish

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,17 +1,17 @@
 <template>
     <section class="max-w-screen-xl mx-auto">
-        <div class="grid grid-cols-8 gap-4 mb-10">
-            <Filter-btn
-                v-for="f in cards"
-                :key="f.id"
-                :filter-text="f.hobby"
-            />
+        <div class="mb-10">
+            <p class="font-serif text-blue text-3xl uppercase">Filter by Hobby:</p>
+            <select v-model="hobbySelect" class="w-1/5 font-sans text-blue">
+                <option disabled selected value>- Select -</option>
+                <option v-for="(hobby, index) in uniqueHobby" :key="index" :value="hobby">{{hobby}}</option>
+            </select>
         </div>
         <div class="flex">
             <div class="bg-green w-7/12">
                 <h1 class="bg-blue font-serif p-5 text-center text-yellow text-3xl uppercase">available Cards</h1>
                 <ul class="grid grid-cols-3 gap-4 mt-10 px-5"> 
-                    <li v-for="card in cards" :key="card.id">
+                    <li v-for="card in filterCards" :key="card.id">
                         <Card :card="card" />
                     </li>
                 </ul>
@@ -34,13 +34,16 @@
 
 import {mapGetters} from 'vuex';
 import Card from './Card.vue';
-import FilterBtn from './FilterBtn.vue';
 
 export default {
   name: 'CardList',
     components: {
-        Card,
-        FilterBtn
+        Card
+    },
+    data() {
+        return {
+            hobbySelect: ""
+        }
     },
     methods: { 
         deselect(selection) {
@@ -57,8 +60,24 @@ export default {
         cards() {
             return this.$store.getters.unselectedCards;
         },
-        filteredCards() {
-            return this.$store.getters.filteredHobby;
+        uniqueHobby: function() {
+            var filtered_hobby = [];
+            var cards = this.$store.getters.unselectedCards;
+            for(var i =0; i < cards.length; i++) {
+                if(filtered_hobby.indexOf(cards[i].hobby) === -1) {
+                    filtered_hobby.push(cards[i].hobby)
+                }
+            }
+            return filtered_hobby;
+        },
+        filterCards: function() {
+            let filtered = this.$store.getters.unselectedCards;
+            if (this.hobbySelect) {
+                filtered = filtered.filter(card => {
+                    return card.hobby == this.hobbySelect;
+                });
+            }
+            return filtered;
         }
     }
 }
--- src/store/getters.js
+++ src/store/getters.js
@@ -4,11 +4,6 @@ export default {
             return !card.selected;
         })
     },
-    filteredHobby(state){
-        return state.cards.filter(card => {
-            return card.hobby == 'Education';
-        })
-    },
     selections(state) {
         return state.selections;
     }
